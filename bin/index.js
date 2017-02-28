3504f2434f2d135f7ae2421ee40c5661f6cfcf99
Kolyaj <kolyaj@yandex.ru>
2017-02-28 07:16:17 +0300

    bugfixes

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,30 @@
 #!/usr/bin/env node
-require('jossy/bin');
+
+var fs = require('fs');
+var dresscode = require('../lib/DressCode');
+
+var context = {};
+var args = [];
+process.argv.slice(2).forEach((arg) => {
+    if (arg.indexOf('-') == 0) {
+        context[arg.substr(1)] = true;
+    } else {
+        args.push(arg);
+    }
+});
+
+if (!args[0]) {
+    console.log('Usage: jossy <input file or dir> <output file or dir> -context_var1 -context_var2 ...');
+    process.exit(1);
+}
+
+var output = args[1] ? fs.createWriteStream(args[1], 'utf8') : process.stdout;
+dresscode(args[0], context, context.debug).then((result) => {
+    output.write(result);
+    if (output != process.stdout) {
+        output.end();
+    }
+}).catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+});
