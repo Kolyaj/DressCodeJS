87c2ec29356fc52670b20cc93d67774ab39f7a09
Kolyaj <kolyaj@yandex.ru>
2020-08-07 18:08:26 +0300

    bugfix: require Foo.bar when usage is Foo.bar.call()

---
+++ test/tests/Foo-bar-forEach.js
@@ -0,0 +1,17 @@
+//=== /js-dev/index.js
+Foo.bar.forEach();
+
+//=== /js-dev/.dresscode
+//: ../js-libs
+
+//=== /js-libs/Foo/index.js
+var Foo = {};
+Foo.bar = [];
+
+//=== /js-libs/.dresscode
+//: .
+
+//===
+var Foo = {};
+Foo.bar = [];
+Foo.bar.forEach();
---
+++ test/tests/autorequire-01.js
@@ -0,0 +1,23 @@
+//=== /js-dev/index.js
+Foo.bar();
+
+//=== /js-dev/.dresscode
+//: ../js-libs
+
+//=== /js-libs/Foo/index.js
+var Foo = {
+    bar: function() {
+
+    }
+};
+
+//=== /js-libs/.dresscode
+//: .
+
+//===
+var Foo = {
+    bar: function() {
+
+    }
+};
+Foo.bar();
